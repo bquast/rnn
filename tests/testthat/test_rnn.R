ee8c5e5f14e3c48a92e87138c3e11f5d700b6fb8
Bastiaan Quast <bquast@gmail.com>
2015-12-02 15:05:49 +0100

    new input method

--- tests/testthat/test_rnn.R
+++ tests/testthat/test_rnn.R
@@ -1,11 +1,16 @@
-# training dataset generation
-binary_dim = 8
+# create training inputs
+x1 = sample(0:127, 7000, replace=TRUE)
+x2 = sample(0:127, 7000, replace=TRUE)
 
-# input variables
-alpha = 0.1
-input_dim = 2
-hidden_dim = 16
-output_dim = 1
+# create training output
+y <- x1 + x2
 
-# run
-rnn(binary_dim, alpha, input_dim, hidden_dim, output_dim)
+# run the 
+rnn(y,
+    x1,
+    x2,
+    binary_dim =  8,
+    alpha      =  0.1,
+    input_dim  =  2,
+    hidden_dim = 10,
+    output_dim =  1)
