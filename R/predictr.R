a103af75b5fad75655df13a2f83f5146b714d419
Bastiaan Quast <bquast@gmail.com>
2016-05-14 13:59:23 +0200

    use nested ifelseif in predictr; test_rnn working

--- R/predictr.R
+++ R/predictr.R
@@ -110,11 +110,9 @@ predictr <- function(model, X, hidden = FALSE, ...) {
       for(i in seq(length(synapse_dim) - 1)){
         if(i == 1){ # first hidden layer, need to take x as input
           layers[[i]] <- logistic((x%*%time_synapse[[i]]) + (layers_values[[i]][dim(layers_values[[i]])[1],] %*% recurrent_synapse[[i]]))
-        }
-        if(i != length(synapse_dim) - 1 & i != 1){ #hidden layers not linked to input layer, depends of the last time step
+        } else if(i != length(synapse_dim) - 1 & i != 1){ #hidden layers not linked to input layer, depends of the last time step
           layers[[i]] <- logistic((layers[[i-1]]%*%time_synapse[[i]]) + (layers_values[[i]][dim(layers_values[[i]])[1],] %*% recurrent_synapse[[i]]))
-        }
-        if(i == length(synapse_dim) - 1){ # output layer depend only of the hidden layer of bellow
+        } else { # output layer depend only of the hidden layer of bellow
           layers[[i]] <- logistic(layers[[i-1]] %*% time_synapse[[i]])
         }
         # storing
