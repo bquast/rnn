4c1ff6e840f3bdab9056435a429df4c8bc0103a3
Bastiaan Quast <bquast@gmail.com>
2022-01-10 12:48:11 +0100

    update URL

--- R/rnn.R
+++ R/rnn.R
@@ -3,5 +3,5 @@
 #' @author Bastiaan Quast \email{bquast@@gmail.com}
 #' @description A Recurrent Neural Network in native R, transforms numbers to binaries before adding bit by bit, teaching itself how to carry.
 #' @seealso \code{\link{trainr}} for training a model and \code{\link{predictr}} for using a model to make predictions. http://qua.st/rnn
-#' @references {http://qua.st/rnn}
+#' @references {http://qua.st/rnn/}
 NULL
